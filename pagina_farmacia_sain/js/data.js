15e7ae439f4b876dfc4e895c07a6647050fff51e
piedracobalto <54822477+piedracobalto@users.noreply.github.com>
2020-06-22 23:46:21 -0300

    Add files via upload
    primer boceto

---
+++ pagina_farmacia_sain/js/data.js
@@ -0,0 +1,57 @@
+var data = {
+        productos:[
+            {"id": 0, 
+            "nombre": "Ibuprofeno", 
+            "categoria": "Medicamentos", 
+            "precio": 300, 
+            "imagen":"../images/imagen-medicamentos1.jpg"},
+
+            {"id": 1, 
+            "nombre": "Tafirol", 
+            "categoria": "Medicamentos", 
+            "precio": 200, 
+            "imagen":"../images/imagen-medicamentos2.jpg"},
+
+            {"id": 2, 
+            "nombre": "Aspirinas", 
+            "categoria": "Medicamentos", 
+            "precio": 250, 
+            "imagen":"../images/imagen-medicamentos3.jpg"},
+
+            {"id": 3, 
+            "nombre": "Afeitaadora Phillips", 
+            "categoria": "Cuidado Personal", 
+            "precio": 1250, 
+            "imagen":"../images/imagen-medicamentos1.jpg"},
+
+            {"id": 4, 
+            "nombre": "Desodorante Axe", 
+            "categoria": "Cuidado Personal", 
+            "precio": 350, 
+            "imagen":"../images/desodorante.jpg"},
+
+            {"id": 5, 
+            "nombre": "Jabon Dove", 
+            "categoria": "Cuidado Personal", 
+            "precio": 200, 
+            "imagen":"../images/jabon.jpg"},
+
+            {"id": 6, 
+            "nombre": "Perfume Dolce y Gavanna", 
+            "categoria": "Perfumeria", 
+            "precio": 2250, 
+            "imagen":"../images/perfume1.jpg"},
+
+            {"id": 7,
+             "nombre": "Perfume Carolina Herrera", 
+            "categoria": "Perfumeria", 
+            "precio": 2550, 
+            "imagen":"../images/perfume2.jpg"},
+
+            {"id": 8,
+             "nombre": "Perfume Coco Chanel", 
+            "categoria": "Perfumeria", 
+            "precio": 2750, 
+            "imagen":"../images/perfume3.jpg"}
+        ]
+};
---
+++ pagina_farmacia_sain/js/main.js
@@ -0,0 +1,8 @@
+let productos = data.productos;
+
+console.log(productos);
+
+function cuadroProducto (producto,id){
+var cuadro = document.getElementById(id);
+let 
+}
